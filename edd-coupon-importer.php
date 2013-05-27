f14722e1d7b4dace859bfb1305b4e7f39964b3cd
Chris Christoff <chriscct7@gmail.com>
2013-05-27 15:34:40 -0400

    Ready for Audit

---
+++ edd-coupon-importer.php
@@ -0,0 +1,115 @@
+<?php
+/*
+Plugin Name: Easy Digital Downloads - Coupon Import
+Plugin URI: http://easydigitaldownloads.com/chris-is-amazing
+Description: Adds the ability to import coupons via a CSV file.
+Author: Chris Christoff
+Author URI: http://www.chriscct7.com
+Version: 1.0
+*/
+
+if ( is_edd_activated() ) {
+
+// plugin folder url
+if(!defined('EDD_COUPON_IMPORT_URL')) {
+	define('EDD_COUPON_IMPORT_URL', plugin_dir_url( __FILE__ ));
+}
+// plugin folder path
+if(!defined('EDD_COUPON_IMPORT_DIR')) {
+	define('EDD_COUPON_IMPORT_DIR', plugin_dir_path( __FILE__ ));
+}
+// plugin root file
+if(!defined('EDD_COUPON_IMPORT_FILE')) {
+	define('EDD_COUPON_IMPORT_FILE', __FILE__);
+}
+
+define( 'EDD_CI_STORE_URL', 'https://easydigitaldownloads.com' ); 
+define( 'EDD_CI', 'Coupon Importer' ); 
+define( 'EDD_CI_VERSION', '1.0' ); 
+
+if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) {
+	include( dirname( __FILE__ ) . '/EDD_SL_Plugin_Updater.php' );
+	}
+
+// retrieve our license key from the DB
+$license_key = trim( get_option( 'edd_ci_license_key' ) );
+
+// setup the updater
+$edd_updater = new EDD_SL_Plugin_Updater( EDD_CI_STORE_URL, __FILE__, array( 
+	'version' 	=> EDD_CI_VERSION, 				// current version number
+	'license' 	=> $license_key, 		// license key (used get_option above to retrieve from DB)
+	'item_name' => EDD_CI, 	// name of this plugin
+	'author' 	=> 'Chris Christoff'  // author of this plugin
+	)
+);
+
+add_action('admin_menu', 'edd_ci_license_menu');
+add_action('admin_init', 'edd_ci_register_option');
+add_action('admin_init', 'edd_ci_deactivate_license');
+add_action('admin_init', 'edd_ci_activate_license');
+			
+if ( is_admin() ) {
+
+	global $edd_import_coupon_file_columns;
+	$edd_import_coupon_file_columns = array(
+		"0" => "A",
+		"1" => "B",
+		"2" => "C",
+		"3" => "D",
+		"4" => "E",
+		"5" => "F",
+		"6" => "G",
+		"7" => "H",
+		"8" => "I",
+		"9" => "J",
+		"10" => "K",
+		"11" => "L",
+		"12" => "M",
+		"13" => "N",
+		"14" => "O",
+		"15" => "P",
+		"16" => "Q",
+		"17" => "R",
+		"18" => "S",
+		"19" => "T",
+		"20" => "U",
+		"21" => "V",
+		"22" => "W",
+		"23" => "X",
+		"24" => "Y",
+		"25" => "Z",
+		"26" => "AA",
+		"27" => "BB",
+		"28" => "CC",
+		"29" => "DD",
+		"30" => "EE",
+		"31" => "FF",
+		"32" => "GG",
+		"33" => "HH",
+		"34" => "II",
+		"35" => "JJ",
+		"36" => "KK",
+		"37" => "LL",
+		"38" => "MM",
+		"39" => "NN",
+		"40" => "OO",
+		"41" => "PP",
+		"42" => "QQ",
+		"43" => "RR",
+		"44" => "SS",
+		"45" => "TT",
+		"46" => "UU",
+		"47" => "VV",
+		"48" => "WW",
+		"49" => "XX",
+		"50" => "YY",
+		"51" => "ZZ"
+	);
+}
+
+include_once( EDD_COUPON_IMPORT_DIR . 'includes/register-settings.php' );
+include_once( EDD_COUPON_IMPORT_DIR . 'includes/admin-actions.php' );
+include_once( EDD_COUPON_IMPORT_DIR . 'includes/admin-page.php' );
+include_once( EDD_COUPON_IMPORT_DIR . 'includes/import-functions.php' );
+include_once( EDD_COUPON_IMPORT_DIR . 'includes/install.php' );
+}
---
+++ includes/import-functions.php
@@ -0,0 +1,306 @@
+<?php
+/**
+ * Import Functions
+ *
+ * @package     Easy Digital Downloads - CSV Import
+ * @subpackage  Import Functions
+ * @copyright   Copyright (c) 2012, Daniel Espinzoa
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0
+*/
+
+
+/**
+ * Show File Upload
+ *
+ * @access      public
+ * @since       1.0
+ * @return      void
+*/
+
+function edd_show_coupon_upload_form() {
+	global $edd_import_field_mapping, $edd_import_coupon_file_columns, $edd_message;
+
+	if ( $edd_message != '') { ?>
+	<div id="" class="updated settings-error">
+		<p><strong><?php echo $edd_message ?></strong></p>
+	</div>
+	<?php } ?>
+	<h3><?php  _e('Import Coupons From A CSV File', 'edd') ?></h3>
+	<p>
+		<?php _e('To import coupons select a valid CSV file and press Import File.  Results  of the import will be displayed in the log area below.', 'edd') ?>
+	</p>
+	<p>
+		<?php
+		$link = '<a href="' . get_admin_url() . 'edit.php?post_type=download&page=edd-settings&tab=misc" >' . __('Coupon Import Mapping', 'edd') . '</a>';
+		echo sprintf( __('The mapping of coupon fields to CSV columns can be adjusted in the %s section.','edd'), $link );
+		?>
+	</p>
+	<form method="post" enctype="multipart/form-data">
+		<table class="form-table">
+		<tbody>
+			<tr valign="top">
+				<th scope="row"><?php _e('Import File','edd') ?></th>
+				<td><input type="file" id="import_file" name="import_file" /></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><?php _e('Validate File Only','edd') ?></th>
+				<td><input type="checkbox" id="check_file" name="check_file" /> <?php _e('Check file, but do not save downloads to the database.','edd') ?></td>
+			</tr>
+		</tbody>
+		</table>
+		<?php
+			$edd_generate_mapping_nonce = wp_create_nonce('edd_generate_mapping');
+		?>
+		<input type="hidden" name="_wpnonce" value="<?php echo $edd_generate_mapping_nonce  ?>" />
+		<input type="hidden" name="edd-action" value="import_coupon_csv" />
+		<p>
+			<input type="submit" name="" id="" class="button-secondary action" value="<?php _e('Import File','edd'); ?>" />
+		</p>
+	</form>
+
+	<?php
+}
+
+
+/**
+ * Show Import Log
+ *
+ * @access      public
+ * @param 		edd_log_file - The log file to display
+ * @since       1.0
+ * @return      void
+*/
+
+function edd_show_coupon_import_log() {
+	global $edd_log_file;
+	?>
+	<strong>Output Log</strong><br/>
+	<textarea style="width: 650px; height: 150px" ><?php echo $edd_log_file ?></textarea>
+	<?php
+}
+
+
+
+/**
+ * Setup Upload Directories
+ *
+ * Patterned after edd_change_downloads_upload_dir() located in includes/upload-functions.php
+ * @access      public
+ * @since       1.0
+ * @return      void
+*/
+function edd_setup_upload_dirs() {
+
+    $wp_upload_dir = wp_upload_dir();
+    $upload_path = $wp_upload_dir['basedir'] . '/edd' . $wp_upload_dir['subdir'];
+
+    // We don't want users snooping in the EDD root, so let's add htaccess there, first
+    // Creating the directory if it doesn't already exist.
+    $rules = 'Options -Indexes';
+    if( ! @file_get_contents( $wp_upload_dir['basedir'] . '/edd/.htaccess' ) ) {
+    	wp_mkdir_p( $wp_upload_dir['basedir'] . '/edd' );
+    } // end if
+    @file_put_contents( $wp_upload_dir['basedir'] . '/edd/.htaccess', $rules );
+
+    // now add blank index.php files to the {year}/{month} directory
+    if ( wp_mkdir_p( $upload_path ) ) {
+
+        $folder = '.';
+        if( !file_exists( $folder . 'index.php' ) ) {
+            @file_put_contents( $folder . 'index.php', '<?php' . PHP_EOL . '// silence is golden' );
+        }
+
+    }
+
+}
+
+/**
+ * Import CSV File
+ *
+ * @access      public
+ * @since       1.0
+ * @return      void
+*/
+function edd_import_coupon_csv_file() {
+	global $edd_options, $edd_log_file;
+
+	$file_name = $_FILES['import_file']['name'];
+	$edd_log_file = sprintf( __('Starting import of file: %s' , 'edd'), $file_name ) . "\n";
+
+	if ( is_file($_FILES['import_file']['tmp_name'])) {
+		if ( ($handle = fopen( $_FILES['import_file']['tmp_name'], "r")) !== FALSE ) {
+
+			$row_ctr=0;
+			while (($line = fgetcsv($handle, 1000,",")) !== FALSE ) {
+
+				$row_ctr++;
+
+				// skip header row
+				if ( $row_ctr > 1 ) {
+
+					// get the current post's data from the mapped column
+					$edd_discount_name				= isset( $edd_options['edd_import_discount_name'] ) 			 ? $line[ $edd_options['edd_import_discount_name'] ]         : $line[0];
+					$edd_discount_code				= isset( $edd_options['edd_import_discount_code'] ) 			 ? $line[ $edd_options['edd_import_discount_code'] ]         : $line[1];
+					$edd_discount_type				= isset( $edd_options['edd_import_discount_type'] ) 			 ? $line[ $edd_options['edd_import_discount_type'] ]         : $line[2];
+					$edd_discount_start				= isset( $edd_options['edd_import_discount_start'] ) 			 ? $line[ $edd_options['edd_import_discount_start'] ]        : $line[3];					
+					$edd_discount_end				= isset( $edd_options['edd_import_discount_end'] ) 				 ? $line[ $edd_options['edd_import_discount_end'] ]          : $line[4];
+					$edd_discount_status			= isset( $edd_options['edd_import_discount_status'] ) 			 ? $line[ $edd_options['edd_import_discount_status'] ]       : $line[5];
+					$edd_discount_uses				= isset( $edd_options['edd_import_discount_uses'] ) 			 ? $line[ $edd_options['edd_import_discount_uses'] ]     	 : $line[6];
+					$edd_discount_max_uses			= isset( $edd_options['edd_import_discount_max_uses'] ) 		 ? $line[ $edd_options['edd_import_discount_max_uses'] ]     : $line[7];
+					$edd_discount_amount			= isset( $edd_options['edd_import_discount_amount'] ) 			 ? $line[ $edd_options['edd_import_discount_amount'] ] 		 : $line[8];
+					$edd_discount_min_price			= isset( $edd_options['edd_import_discount_min_price']) 		 ? $line[ $edd_options['edd_import_discount_min_price'] ]    : $line[9];
+					$edd_discount_product_reqs		= isset( $edd_options['edd_import_discount_product_reqs']) 		 ? $line[ $edd_options['edd_import_discount_product_reqs'] ]    : $line[10];		
+					$edd_discount_product_condition	= isset( $edd_options['edd_import_discount_product_condition'])  ? $line[ $edd_options['edd_import_discount_product_condition'] ]    : $line[11];
+					$edd_discount_is_not_global		= isset( $edd_options['edd_import_discount_is_not_global']) 	 ? $line[ $edd_options['edd_import_discount_is_not_global'] ]    : $line[12];
+					$edd_discount_is_single_use		= isset( $edd_options['edd_import_discount_is_single_use']) 	 ? $line[ $edd_options['edd_import_discount_is_single_use'] ]    : $line[13];
+					$edd_discount_id				= isset( $edd_options['edd_import_discount_id']) 				 ? $line[ $edd_options['edd_import_discount_id'] ]    : $line[14];
+
+					
+					// check for valid data
+					$validation_error = false;
+					$validation_msg  = '';
+
+					// discount name  blank
+					if ( 0 == strlen( $edd_discount_name ) ) {
+						$validation_error = true;
+						$validation_msg  .= __('Discount Name is blank.', 'edd') . "; ";
+					}
+					
+					// discount code  blank
+					if ( 0 == strlen( $edd_discount_code ) ) {
+						$validation_error = true;
+						$validation_msg  .= __('Discount code is blank.', 'edd') . "; ";
+					}
+					
+					// discount type is blank
+					if ( 0 == strlen( $edd_discount_type ) ) {
+						$validation_error = true;
+						$validation_msg  .= __('Discount type is blank.', 'edd') . "; ";
+					}
+					
+					// discount type is invaid
+					if ( ( $edd_discount_type != 'flat' ) && ( $edd_discount_type != 'percentage' ) ) {
+						$validation_error = true;
+						$validation_msg  .= __('Discount type is invalid.', 'edd') . "; ";
+					}
+					
+					// discount code is blank
+					if ( 0 == strlen( $edd_discount_code ) ) {
+						$validation_error = true;
+						$validation_msg  .= __('Discount code is blank.', 'edd') . "; ";
+					}
+					
+					// start time validation
+					if ( ! strtotime( $edd_discount_start ) ) {
+						$validation_error = true;
+						$validation_msg  .= sprintf(__('Date validation failed for string: %s', 'edd'), $edd_discount_start) . "; ";
+					}
+					
+					// end time validation
+					if ( ! strtotime( $edd_discount_end ) ) {
+						$validation_error = true;
+						$validation_msg  .= sprintf(__('Date validation failed for string: %s', 'edd'), $edd_discount_end) . "; ";
+					}
+					
+					// set start time to correct date type
+					$edd_discount_start =  date( 'm/d/Y H:i:s', strtotime( $edd_discount_start ) );
+					
+					// set end time to the correct date type
+					$edd_discount_end =  date( 'm/d/Y H:i:s',  strtotime(  date( 'm/d/Y', strtotime( $edd_discount_end ) ) . ' 23:59:59' )  );
+				
+					// check status 
+					$status = array( 'active', 'inactive', 'expired' );
+					if ( ! in_array( $edd_discount_status, $status ) ) {
+						$validation_error = true;
+						$validation_msg  .= __('Status needs to be active, inactive, or pending', 'edd') . "; ";
+					}
+					
+					// maybe more validation later
+
+
+					if ( $validation_error ) {
+						// report error and loop
+						$edd_log_file .= sprintf( __('Error importing row %d: ', 'edd'), $row_ctr)  . $validation_msg . "\n" ;
+					} else {
+
+						if ( isset($_POST['check_file'] ) && ( $_POST['check_file'] == 'on' ) ) {
+
+							$edd_log_file .= sprintf(__('Successfully validated row: %d', 'edd'), $row_ctr ) . "\n";
+
+						} else {
+								
+							$meta = array(
+									'code'       		=> $edd_discount_code,
+									'uses'       		=> $edd_discount_uses,
+									'max_uses'   		=> $edd_discount_max_uses,
+									'amount'     		=> $edd_discount_amount,
+									'start'      		=> $edd_discount_start,
+									'expiration' 		=> $edd_discount_end,
+									'type'       		=> $edd_discount_type,
+									'min_price'   		=> $edd_discount_min_price,
+									'product_reqs'      => $edd_discount_min_price,
+									'product_condition' => $edd_discount_product_reqs,
+									'is_not_global'     => $edd_discount_is_not_global,
+									'is_single_use'     => $edd_discount_is_single_use
+							);
+						
+							// Is it already made?
+							if ( ( 0 == strlen( $edd_discount_id ) ) && edd_discount_exists( $edd_discount_id ) ){
+							
+								wp_update_post( array(
+									'ID'          => $edd_discount_id,
+									'post_title'  => $edd_discount_name,
+									'post_status' => $edd_discount_status
+								) );
+
+								foreach( $meta as $key => $value ) {
+									update_post_meta( $edd_discount_id , '_edd_discount_' . $key, $value );
+								}
+
+								
+							}
+							else {
+							
+								// add post
+								// setup post args
+								$new_download_args = array(
+									'post_type' 	 => 'edd_discount',
+									'post_title' 	 => $edd_discount_name,
+									'post_status' 	 => $edd_discount_status,
+								);
+								$new_download_id = wp_insert_post( $new_download_args , true );
+							
+
+								foreach ( $meta as $key => $value ) {
+									update_post_meta( $new_download_id, '_edd_discount_' . $key, $value );
+								}
+							
+							}
+
+							if ( is_wp_error( $new_download_id ) ) {
+
+								$edd_log_file .= sprintf( __('Error importing row %d: ', 'edd'), $row_ctr) . $new_download_id->get_error_message() ;
+
+							}
+
+						} // if ( just validating )
+
+					} // if ( $validation_error )
+
+				} // if ( $row_ctr )
+
+			} // while()
+
+		} else {
+
+			$edd_log_file .= __('Error opening file uploaded.','edd');
+		}
+
+	} else {
+
+		$edd_log_file .= __('Error opening file uploaded.', 'edd');
+	}
+
+
+}
